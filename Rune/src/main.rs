7efa8471e0ce7f55fc856bc2eca2f55fc0713724
Blank9999 <nahan7799@gmail.com>
2025-04-03 19:36:15 -0400

    Made an initial commit

--- Rune/src/main.rs
+++ Rune/src/main.rs
@@ -1,18 +1,20 @@
 mod lexer;  // Import the lexer module
+mod literal;
 
-use crate::lexer::{Lexer, Token}; // Bring Lexer and Token into sco
+use crate::lexer::{Lexer, Token}; // Bring Lexer and Token into scope
+
 
 fn main() {
-    let source_code = r#"
+    let source_code: &str = r#"
         Int x = 42
         String name = "Ayaan"
         Bool flag = true
         if x == 1 { return }
     "#;
 
-    let mut lexer = Lexer::new(source_code);
+    let mut lexer: Lexer<'_> = Lexer::new(source_code);
     loop {
-        let token = lexer.next_token();
+        let token: Token = lexer.next_token();
         println!("{:?}", token);
         if token == Token::Eof {
             break;
