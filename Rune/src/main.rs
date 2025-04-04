775ec09bfc3dce46a0ff5ea4486a532e2ae96e84
Blank9999 <nahan7799@gmail.com>
2025-04-03 22:28:40 -0400

    Implementing simple variable declaration for a single line

--- Rune/src/main.rs
+++ Rune/src/main.rs
@@ -1,25 +1,36 @@
 mod lexer;  // Import the lexer module
+mod parser;
+mod ast;  
 
-use crate::lexer::{Lexer, Token}; // Bring Lexer and Token into scope
+use crate::lexer::{Lexer,Token}; // Bring Lexer and Token into scope
+use crate::parser::{Parser};
 
 
 fn main() {
 
-    let source_code = r#"
-        int x = 42
-        string name = "Ayaan"
-        # nefioenfioewnfioewnfewio
-        bool flag = true
-        if x == 1 { return } # yooo what dsdbubseuide
-        list<int, string>
-    "#;
+    // let source_code = r#"
+    //     int x = 42
+    //     string name = "Ayaan"
+    //     # nefioenfioewnfioewnfewio
+    //     bool flag = true
+    //     if x == 1 { return } # yooo what dsdbubseuide
+    //     list<int, string>
+    // "#;
+
+    let source_code = "int x = 4";
 
     let mut lexer: Lexer<'_> = Lexer::new(source_code);
-    loop {
-        let token: Token = lexer.next_token();
-        println!("{:?}", token);
-        if token == Token::Eof {
-            break;
-        }
-    }
+    let mut parser = Parser::new(lexer);
+    let decl = parser.parse_declaration();
+    println!("{:#?}", decl);
+
+
+
+    // loop {
+    //     let token: Token = lexer.next_token();
+    //     println!("{:?}", token);
+    //     if token == Token::Eof {
+    //         break;
+    //     }
+    // }
 }
