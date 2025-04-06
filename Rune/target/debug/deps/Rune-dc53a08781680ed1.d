6a21a1d23977306af85c8cff897cd6c2e59492a7
Blank9999 <nahan7799@gmail.com>
2025-04-06 02:42:29 -0400

    Apply .gitignore

--- Rune/target/debug/deps/Rune-dc53a08781680ed1.d
+++
@@ -1,8 +0,0 @@
-/Users/ayaan/Desktop/Rune/Rune/target/debug/deps/Rune-dc53a08781680ed1: src/main.rs src/lexer.rs src/parser.rs src/ast.rs
-
-/Users/ayaan/Desktop/Rune/Rune/target/debug/deps/Rune-dc53a08781680ed1.d: src/main.rs src/lexer.rs src/parser.rs src/ast.rs
-
-src/main.rs:
-src/lexer.rs:
-src/parser.rs:
-src/ast.rs:
